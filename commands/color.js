8ace32996d30a0d9928ea878d68fbe90d0b9608a
kneewoah <odogsimp13@gmail.com>
2020-04-15 12:48:44 -0400

    idk

--- commands/color.js
+++ commands/color.js
@@ -2,28 +2,26 @@ const config = require("../config.json");
 
 exports.run = async (client, message, args) => {
 
-  var author = `${message.author}`;
-
   var query;
   if (args[0].match(/^#(?:[0-9a-fA-F]{6})$/g)) {
-    query = args[0]
-    changeColor(query, author);
+    changeColor(args[0], message.author);
   } else if (args[0].match(/^(?:[0-9a-fA-F]{6})$/g)) {
-    query = args[0].substring(1);
-    changeColor(query, author);
+    changeColor(args[0].substring(1);, message.author);
   } else {
     message.channel.send("Please enter a 6 digit hex code. You can select a color here: <https://htmlcolorcodes.com/color-picker/>");
   }
 
-function changeColor(color, who) {
-  deleteRole(who.id);
-  setTimeout(function() {makeRole(color, who.id)}, 400);
-  setTimeout(function() {addRole(who.id)}, 1600);
+function changeColor(color, author) {
+  deleteRole(author.id);
+  setTimeout(function() {makeRole(color, author.id)}, 400);
+  setTimeout(function() {addRole(author)}, 1600);
 };
 
 function deleteRole(roleName) {
-  if (message.guild.roles.find(role => role.name === roleName)) message.guild.roles.find(role => role.name === roleName).delete();
-  message.channel.send("Role deleted");
+  if (message.guild.roles.find(role => role.name === roleName)) {
+    message.guild.roles.find(role => role.name === roleName).delete();
+    message.channel.send("Role deleted");
+  }
 };
 
 function makeRole(color, id) {
@@ -36,9 +34,9 @@ function makeRole(color, id) {
   message.channel.send("Role maked");
 };
 
-function addRole(roleName) {
-  let roleID = message.guild.roles.find(role => role.name === roleName).id;
-  message.member.addRole(roleID);
+function addRole(user) {
+  let roleID = message.guild.roles.find(role => role.name === user.id).id;
+  user.addRole(roleID);
 };
 
 };
