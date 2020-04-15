114c3977058cf20a35af6bfb70f21743e8fcd6e0
kneewoah <42323086+kneewoah@users.noreply.github.com>
2020-04-14 22:11:43 -0400

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,1 +1,66 @@
+// BEFORE LAUNCH
+const Discord = require('discord.js');
+const client = new Discord.Client({disableEveryone: true});
+const config = require("./config.json");
+const moment = require('moment')
+const timestamp = moment().format('HH:mm:ss');
 
+
+// ON READY
+client.on("ready", () => {
+
+  const owner = client.users.find(user => user.id === config.ownerID)
+  console.log(`${timestamp} "Pillows Bot" ignition sequence has commenced.`);
+});
+
+
+// ON MESSAGE
+client.on("message", async message => {
+
+  if(message.author.bot || message.content.indexOf(config.prefix) !== 0) return;
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  let msgGuild;
+  if(!message.guild) {
+    msgGuild = "Direct Messages";
+  } else {
+    msgGuild = message.guild.name;
+  };
+
+  console.log(`${timestamp}: Attempting to process \'${command}\' sent by user \'${message.author.tag}\' in \'${msgGuild}\'...`)
+
+  try {
+    let commandFile = require(`./commands/${command}.js`)
+
+    commandFile.run(client, message, args);
+    console.log(`${timestamp}: Processed command \'${command}\' succesfully.`);
+    message.react("☁")
+  } catch (error) {
+    //console.log(error);
+    console.log(`${timestamp}: Could not process command \'${command}\'.`);
+  }
+
+});
+// USER JOIN
+
+client.on("guildMemberAdd", member => {
+
+  let roleName = `${member.id}`
+  if(member.guild.roles.find(role => role.name === roleName)) {
+    var roleID = member.guild.roles.find(role => role.name === roleName).id;
+    member.addRole(roleID);
+  } else {
+    const channel = member.guild.channels.find(ch => ch.id === "434701823358795782" || ch.id === "591095105709015053" || ch.name === "general");
+    if (!channel) return;
+    channel.send(`Welcome ${member.displayName}! Type \`!color #HEXCODEHERE\` to chose your role color! You can choose a color here: <https://tr.im/hexwheel>.`);
+  };
+});
+
+// ERROR
+
+client.on('error', console.error);
+
+// TOKEN
+client.login(xxxx);
