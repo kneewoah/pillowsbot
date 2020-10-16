cd9b118655dbf8c6cba5e4c886b4056c8273c308
kneewoah <42323086+kneewoah@users.noreply.github.com>
2020-10-16 17:17:29 -0400

    Add files via upload

---
+++ commands/xp.js
@@ -0,0 +1,20 @@
+const config = require("../config.json");
+
+exports.run = (client, message, args, con) => {
+
+  let target = message.author; //message.mentions.users.first() || message.guild.members.get(args[1]) ||
+
+  con.query(`SELECT * FROM xp WHERE id = '${target.id}'`, (err, rows) => {
+    if(err) throw err;
+
+    if(!rows[0]) return message.channel.send("this user has no XP.");
+    let xp = rows[0].xp;
+    message.channel.send(xp);
+  });
+
+}
+
+exports.help = {
+  description: "how likely you are to get into a school",
+  usage: "!chanceme <school>"
+};
